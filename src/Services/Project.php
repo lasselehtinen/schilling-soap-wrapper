d0cc1ab5b2d17dc34b62890288baab3235d3e53b
Lasse Lehtinen <lasse.lehtinen@iki.fi>
2016-03-03 13:34:44 +0200

    Added saveProjects

--- src/Services/Project.php
+++ src/Services/Project.php
@@ -5,7 +5,7 @@ use LasseLehtinen\SchillingSoapWrapper\SchillingSoapWrapper;
 class Project extends SchillingSoapWrapper
 {
     /**
-     * getProjects
+     * Used to retrieve one or more Project from Schilling
      * @param  array $arguments
      * @return array
      */
@@ -13,5 +13,16 @@ class Project extends SchillingSoapWrapper
     {
         $query = ['WdProjectCriteria' => $arguments];
         return $this->request('Project', 'GetProjects', 'GetProjectsRequest', $query);
+    }
+
+    /**
+     * Used to create/update Projects in Schilling.
+     * @param  array $arguments
+     * @return array
+     */
+    public function saveProjects($arguments)
+    {
+        $query = ['Project' => $arguments];
+        return $this->request('Project', 'SaveProjects', 'SaveProjectsRequest', $query);
     }
 }
